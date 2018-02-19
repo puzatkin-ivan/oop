c1c66305500e03b56d17b28e67f79bc4d097d8fa
puzatkin-ivan <puzatkin1998@gmail.com>
2018-02-20 01:30:00 +0300

    Использовал вариация метода append для замены строки #1

--- lab1/replace/test.bat
+++ lab1/replace/test.bat
@@ -1,38 +1,38 @@
 set PROGRAM="%~1"
 
 rem провереяем запуск без параметров командной строки
-%PROGRAM% > response.txt
-IF NOT ERRORLEVEL 1 GOTO error
+%PROGRAM% > log.txt
+if not errorlevel 1 goto error
 
 rem Проверяем запуск с входным и выходным файлом
-%PROGRAM% undefined.txt %TEMP%\out.txt > response.txt
-IF NOT ERRORLEVEL 1 GOTO error
+%PROGRAM% undefined.txt %TEMP%\out.txt > log.txt
+if not errorlevel 1 goto error
 
-%PROGRAM% name-list.txt "" Arthur Ivan > response.txt
-IF NOT ERRORLEVEL 1 GOTO error
+%PROGRAM% name-list.txt "" Arthur Ivan > log.txt 
+if not errorlevel 1 goto error
 
-%PROGRAM% name-list.txt %TEMP%\out.txt "" Arthur  > response.txt
-IF NOT ERRORLEVEL 1 GOTO error
+%PROGRAM% name-list.txt %TEMP%\out.txt "" Arthur > log.txt  
+if not errorlevel 1 goto error
 
-%PROGRAM% name-list.txt %TEMP%\out.txt Charlotte Anna  > response.txt
-IF ERRORLEVEL 1 GOTO error
-FC %TEMP%\out.txt replace-Charlotte-by-Anna.txt
-if ERRORLEVEL 1 GOTO error 
+%PROGRAM% name-list.txt out.txt Charlotte Anna > log.txt  
+if errorlevel 1 goto error
+fc out.txt replace-Charlotte-by-Anna.txt
+if errorlevel 1 goto error 
 
-%PROGRAM% name-list.txt %TEMP%\out.txt Barbara "" > response.txt
-IF ERRORLEVEL 1 GOTO error
-FC %TEMP%\out.txt without-Barbara.txt
-if ERRORLEVEL 1 GOTO error
+%PROGRAM% name-list.txt out.txt Barbara "" > log.txt
+if errorlevel 1 goto error
+fc out.txt without-Barbara.txt
+if errorlevel 1 goto error
 
-%PROGRAM% replay.txt %TEMP%\out.txt ma mama > response.txt
-IF ERRORLEVEL 1 GOTO error
-FC %TEMP%\out.txt check-by-replay.txt
-if ERRORLEVEL 1 GOTO error
+%PROGRAM% replay.txt %TEMP%\out.txt ma mama > log.txt
+if errorlevel 1 goto error
+fc %TEMP%\out.txt check-by-replay.txt
+if errorlevel 1 goto error
 
 
-ECHO Program testing succeeded
-EXIT 0
+echo Program testing succeeded
+exit 0
 
 :error
-ECHO Test failed
-EXIT 1
+echo Test failed
+exit 1
